7a63d698286768f9b596a2fc54fb90eeb65a81d9
UzzielSW <brayanpuyol@gmail.com>
2025-08-10 22:30:02 -0500

    Actualiza el iframe en pageP1.py

--- pages/pageP1.py
+++ pages/pageP1.py
@@ -9,4 +9,4 @@ st.image("img/DP1-1.png")
 st.image("img/DP1-2.png")
 st.image("img/DP1-3.png")
 
-st.markdown('<iframe src="https://myup3745.autodesk360.com/shares/public/SH286ddQT78850c0d8a4eb5b061e77978ccd?mode=embed" width="640" height="480" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"  frameborder="0"></iframe>', unsafe_allow_html=True)
+st.markdown('<iframe src="https://myup3745.autodesk360.com/shares/public/SH286ddQT78850c0d8a43851c5559bf93dc6?mode=embed" width="640" height="480" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"  frameborder="0"></iframe>', unsafe_allow_html=True)
